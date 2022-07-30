e887f319483d97096f75b03f9ddcc6ac84e40e4e
SpotlightForBugs <mail@SpotlightForBugs.eu>
2022-07-30 14:42:30 +0200

    ermittlung, wer "raus" ist

--- app.py
+++ app.py
@@ -342,9 +342,34 @@ def warten():     # function for the wait function
             anzahl_stimmen = sum(1 for line in text)
             
         text.close()
+        print(anzahl_stimmen)
+        print(i)
         if i == anzahl_stimmen:
             print("Alle Spieler haben gewaehlt")
-            return render_template("Dashboards/status/ergebnis.html")
+            count = 0;  
+            word = "";  
+            maxCount = 0;  
+            words = [];  
+            
+            file = open("abstimmung.txt", "r")  
+                
+            for line in file:  
+                string = line.lower().replace(',','').replace('.','').split(" ");  
+                for s in string:  
+                    words.append(s);  
+            
+            for i in range(0, len(words)):  
+                count = 1;  
+                for j in range(i+1, len(words)):  
+                    if(words[i] == words[j]):  
+                        count = count + 1;  
+                        
+                if(count > maxCount):  
+                    maxCount = count;  
+                    word = words[i];  
+            
+            
+            return render_template("Dashboards/status/ergebnis.html",name=word)
         else: 
              return render_template("Dashboards/status/warten.html")
         

76cf3d6ba3eb2ce40d8184c38077d829060fb08f
SpotlightForBugs <mail@spotlightforbugs.eu>
2022-08-22 08:13:04 +0200

    FLK-E265

--- app.py
+++ app.py
@@ -1150,7 +1150,7 @@ def heilen(name, rolle, auswahl):
     return render_template("fehler.html")
 
 
-#TODO: #76 Sicherheitsabfrage bei der Anzeige des Logs
+# TODO: #76 Sicherheitsabfrage bei der Anzeige des Logs
 @app.route("/log")
 def log_ansehen():
     """

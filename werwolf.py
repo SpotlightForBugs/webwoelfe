707982be90c321c240f1171691048bfc9b65aaa6
SpotlightForBugs <mail@spotlightforbugs.eu>
2022-09-07 11:47:27 +0200

    docs

--- werwolf.py
+++ werwolf.py
@@ -1007,6 +1007,16 @@ def ist_token_vorhandem(name, rolle):
 
 
 def setze_status(token: str, status: str):
+    """
+    The setze_status function sets the status of a user to active or inactive.
+    It takes two arguments: token and status. The token is the unique identifier for each user, 
+    and it is used to find the correct line in tokens.txt where their information is stored. 
+    The status argument can be either &quot;active&quot; or &quot;inactive&quot;. If it's set to active, then all lines containing that token will have their last column changed from an inactive string (e.g., 'inactive') to an active string (e.g., 'active'). If it's set to inactive, then all lines containing that token will have their
+    
+    :param token:str: Check if the token is in the file
+    :param status:str: Set the status of a user
+    :return: Nothing
+    """
     # read the file tokens.txt and check if the token is in the file
     with open("tokens.txt", "r") as file:
         for line in file:
@@ -1041,6 +1051,16 @@ def setze_status(token: str, status: str):
 
 
 def setze_status_fuer_rolle(rolle: str, status: str):
+    """
+    The setze_status_fuer_rolle function sets the status of a role to active or inactive.
+    It takes two arguments: rolle and status. The function reads the file tokens.txt and checks if 
+    the token is in the file, then it splits the line at + signs, returning only name, role and 
+    status from that line.
+    
+    :param rolle:str: Specify the role that should be set to a specific status
+    :param status:str: Set the status of a role
+    :return: The status of the specified role
+    """
     # read the file tokens.txt and check if the token is in the file
     with open("tokens.txt", "r") as file:
         for line in file:
@@ -1075,6 +1095,15 @@ def setze_status_fuer_rolle(rolle: str, status: str):
 
 
 def setze_status_fuer_name(name: str, status: str):
+    """
+    The setze_status_fuer_name function sets the status of a user to active or inactive.
+    It takes two arguments: name and status. The function reads the file tokens.txt and checks if 
+    the token is in the file, then it splits the line at + and returns name and role.
+    
+    :param name:str: Specify the name of the user that should be changed
+    :param status:str: Set the status of a user
+    :return: None
+    """
     # read the file tokens.txt and check if the token is in the file
     with open("tokens.txt", "r") as file:
         for line in file:
@@ -1109,6 +1138,14 @@ def setze_status_fuer_name(name: str, status: str):
 
 
 def setze_status_fuer_alle(status: str):
+    """
+    The setze_status_fuer_alle function sets the status of all roles to a given value.
+    It reads the file tokens.txt and checks if the token is in the file, then it sets 
+    the status for that role to a given value.
+    
+    :param status:str: Set the status for all roles to a given value
+    :return: None
+    """
     # read the file tokens.txt and check if the token is in the file
     with open("tokens.txt", "r") as file:
         for line in file:
@@ -1136,6 +1173,13 @@ def setze_status_fuer_alle(status: str):
 
 
 def actions(action: str):
+    """
+    The actions function is used to set the status of all players.
+    The following order is used: 0 = dead, 1 = sleep, 2 = action, 3 = vote, 4 = information
+    
+    :param action:str: Call the function actions(action:str)
+    :return: The following:
+    """
     # es gibt verschiedene Stadien in denen sich ein Spieler befinden kann. Er kann schlafen, tot sein,eine information bekommen, eine Aktion haben oder Abstimmung haben.
     # Die Funktionen setze_status_fuer_rolle, setze_status_fuer_name und setze_status setzen den Status eines Spielers auf einen der oben genannten Stadien.
     # The following order is used: 0 = dead, 1 = sleep, 2 = action, 3 = vote, 4 = information, 5 = idle
@@ -1202,6 +1246,14 @@ def actions(action: str):
 
 
 def erhalte_ziel(token: str)->str:
+    """
+    The erhalte_ziel function returns the URL of the dashboard for a given player.
+    The function takes one argument, which is a token string.
+    
+    
+    :param token:str: Get the role of the player
+    :return: The path to the next page of the player
+    """
     # 1. get the role of the player
     rolle = rolle_aus_token(token)
 
@@ -1228,6 +1280,14 @@ def erhalte_ziel(token: str)->str:
 
 
 def nurNamen() -> list:
+    """
+    The nurNamen function creates a list of all the names in the log file.
+    It does this by reading every line in the log file and checking if it contains a * or not.
+    If it does contain a *, nothing happens. If it doesn't contain one, then we split at = and take 
+    the first part of that line (which is always the name) and append that to our nurNamen list.
+    
+    :return: A list with the names of all players that are not dead or the narrator
+    """
     with open("rollen_log.txt") as players_log:  # open the log file
         players_log = players_log.readlines()  # read the log file
 

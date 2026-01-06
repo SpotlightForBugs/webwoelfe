b872623f7ea8fc583159fb102d8f59522ec635e8
Johannes Häusler <mail@spotlightforbugs.eu>
2026-01-06 13:38:53 +0100

    Remove duplicate roles: Bürgermeister and Selbstmörder from spezial folder

--- roles/spezial/__init__.py
+++ roles/spezial/__init__.py
@@ -4,10 +4,8 @@ Spezialrollen Paket.
 Einzigartige Rollen mit besonderen Mechaniken.
 """
 
-from .buergermeister import Buergermeister
 from .hure import Hure
 
 __all__ = [
-    'Buergermeister',
     'Hure',
 ]
